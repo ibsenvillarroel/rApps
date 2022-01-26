257eb41f9fa5d8b36b6b297d1b3ea072918f1a38
Ibsen Villarroel <ibsenvillarroel@hotmail.com>
2022-01-26 00:31:21 -0400

    Update Name App.r

--- Name App/Name App.r
+++ Name App/Name App.r
@@ -1,1 +1,43 @@
+# Load R packages
+library(shiny)
+library(shinythemes)
 
+
+# Define UI
+ui <- fluidPage(theme = shinytheme("cerulean"),
+                navbarPage(
+                  # theme = "cerulean",  # <--- To use a theme, uncomment this
+                  "Aplicacion en R",
+                  tabPanel("Pestana 1",
+                           sidebarPanel(
+                             tags$h3("Datos:"),
+                             textInput("txt1", "Primer Nombre:", ""),
+                             textInput("txt2", "Primer Apellido:", ""),
+                             
+                           ), # sidebarPanel
+                           mainPanel(
+                             h1("App de nombres en R"),
+                             
+                             h4("Nombre y Apellido"),
+                             verbatimTextOutput("txtout"),
+                             
+                           ) # mainPanel
+                           
+                  ), # Navbar 1, tabPanel
+                  tabPanel("Segunda pestana", "Podemos colocar un readme o una explicacion del proyecto")
+                  
+                ) # navbarPage
+) # fluidPage
+
+
+# Define server function  
+server <- function(input, output) {
+  
+  output$txtout <- renderText({
+    paste( input$txt1, input$txt2, sep = " " )
+  })
+} # server
+
+
+# Create Shiny object
+shinyApp(ui = ui, server = server)
